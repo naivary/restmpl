140fcb6811c728d03a8710e2b99d97057f7eb683
Sayed Mustafa Hussaini <SayedMustafaHussaini@outlook.de>
2023-05-24 09:39:27 +0200

    decided to save the filestore interface first

--- internal/app/fs/fs.go
+++ internal/app/fs/fs.go
@@ -1,28 +1,12 @@
 package fs
 
 import (
-	"github.com/google/uuid"
 	"github.com/knadh/koanf/v2"
-	"github.com/naivary/instance/internal/pkg/env"
 	"github.com/naivary/instance/internal/pkg/filestore"
 )
-
-var _ env.Env = (*Env)(nil)
 
 type Env struct {
 	K *koanf.Koanf
 
 	Store filestore.Store
-}
-
-func (e Env) Name() string {
-	return "fs"
-}
-
-func (e Env) ID() string {
-	return uuid.NewString()
-}
-
-func (e Env) Routername() string {
-	return "/fs"
 }
