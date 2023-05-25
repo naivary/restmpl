77a5373c52f9edbf91b11f89291404843eb6fea5
Sayed Mustafa Hussaini <SayedMustafaHussaini@outlook.de>
2023-05-25 12:12:53 +0200

    now working

--- internal/pkg/ctrl/wire.go
+++ internal/pkg/ctrl/wire.go
@@ -4,6 +4,7 @@
 package ctrl
 
 import (
+	"github.com/go-chi/chi/v5"
 	"github.com/google/wire"
 
 	"github.com/naivary/instance/internal/app/fs"
@@ -14,19 +15,27 @@ import (
 	"github.com/naivary/instance/internal/pkg/filestore"
 	"github.com/naivary/instance/internal/pkg/models/metadata"
 	"github.com/naivary/instance/internal/pkg/routes"
-	"github.com/naivary/instance/internal/pkg/services"
+	"github.com/naivary/instance/internal/pkg/service"
 )
 
 var (
-	db     = wire.NewSet(database.Connect)
-	svc    = wire.NewSet(wire.Struct(new(sys.Sys), "*"), wire.Struct(new(fs.Fs), "*"), wire.Struct(new(services.Services), "*"))
-	app    = wire.Struct(new(ctrl.API), "*")
-	httpFs = wire.NewSet(filestore.New, wire.Bind(new(filestore.Store), new(filestore.Filestore)))
-	k      = wire.NewSet(config.New)
-	m      = wire.NewSet(metadata.New)
+	db         = wire.NewSet(database.Connect)
+	svcs       = wire.NewSet(wire.Struct(new(sys.Sys), "*"), wire.Struct(new(fs.Fs), "*"))
+	api        = wire.Struct(new(ctrl.API), "*")
+	httpFs     = wire.NewSet(filestore.New, wire.Bind(new(filestore.Store), new(filestore.Filestore)))
+	rootRouter = wire.NewSet(routes.New)
+	k          = wire.NewSet(config.New)
+	m          = wire.NewSet(metadata.New)
 )
 
-func New() (*ctrl.API, error) {
-	wire.Build(db, svc, routes.New, app, k, m, httpFs)
+func allSvcs(sys *sys.Sys, fs *fs.Fs) []service.Service[chi.Router] {
+	return []service.Service[chi.Router]{
+		sys,
+		fs,
+	}
+}
+
+func New(cfgFile string) (*ctrl.API, error) {
+	wire.Build(db, svcs, rootRouter, api, k, m, httpFs, allSvcs)
 	return &ctrl.API{}, nil
 }
