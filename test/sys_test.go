8a3bd36df89d5416509cfeb5939e97c4d0a91c1f
Sayed Mustafa Hussaini <SayedMustafaHussaini@outlook.de>
2023-05-25 10:52:26 +0200

    this way of testing should be the right one

--- test/sys_test.go
+++ test/sys_test.go
@@ -1,16 +1,17 @@
 package test
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func TestHealth(t *testing.T) {
-	r := httptest.NewRequest(http.MethodGet, "/sys", nil)
+	r := httptest.NewRequest(http.MethodGet, "/sys/health", nil)
 	w := httptest.NewRecorder()
-	hf := http.HandlerFunc(api.Services.Sys.Health)
-	hf.ServeHTTP(w, r)
+	api.Router.ServeHTTP(w, r)
+	fmt.Println(w.Body)
 	if w.Result().StatusCode != 200 {
 		t.Errorf("statuscode was not 200. Got: %d", w.Result().StatusCode)
 	}
