71de7caa4925891e3df74422bcf9518b0b1220d0
Sayed Mustafa Hussaini <SayedMustafaHussaini@outlook.de>
2023-05-25 17:19:25 +0200

    testing finished. I should write a util test package which keeps handy functions like equal mustOpen ...

--- internal/app/sys/sys_test.go
+++ internal/app/sys/sys_test.go
@@ -1,10 +1,15 @@
 package sys
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"os"
+	"reflect"
 	"testing"
 
 	"github.com/naivary/instance/internal/pkg/config"
@@ -50,6 +55,31 @@ func setupTestServer() *httptest.Server {
 	return httptest.NewServer(root)
 }
 
+func mustOpen(path string) *os.File {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return file
+}
+
+func areEqualJSON(s1, s2 string) (bool, error) {
+	var o1 interface{}
+	var o2 interface{}
+
+	var err error
+	err = json.Unmarshal([]byte(s1), &o1)
+	if err != nil {
+		return false, fmt.Errorf("Error mashalling string 1 :: %s", err.Error())
+	}
+	err = json.Unmarshal([]byte(s2), &o2)
+	if err != nil {
+		return false, fmt.Errorf("Error mashalling string 2 :: %s", err.Error())
+	}
+
+	return reflect.DeepEqual(o1, o2), nil
+}
+
 func TestHealth(t *testing.T) {
 	c := ts.Client()
 	url, err := url.JoinPath(ts.URL, "sys", "health")
@@ -63,5 +93,28 @@ func TestHealth(t *testing.T) {
 
 	if res.StatusCode != http.StatusOK {
 		t.Fatalf("Expected status code to be %d. Got: %d while sending request to: %s", http.StatusOK, res.StatusCode, url)
+	}
+
+	got := new(bytes.Buffer)
+	expected := new(bytes.Buffer)
+
+	_, err = got.ReadFrom(res.Body)
+	if err != nil {
+		t.Error(err)
+	}
+
+	file := mustOpen("./testdata/health.json")
+	_, err = expected.ReadFrom(file)
+	if err != nil {
+		t.Error(err)
+	}
+	buf := new(bytes.Buffer)
+	err = json.Compact(buf, expected.Bytes())
+	if err != nil {
+		t.Error(err)
+	}
+
+	if ok, err := areEqualJSON(buf.String(), got.String()); !ok || err != nil {
+		t.Fatalf("Should be equal: %v", err)
 	}
 }
