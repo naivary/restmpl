8e784df66845ec1d8100df938185400be55c23ec
Sayed Mustafa Hussaini <SayedMustafaHussaini@outlook.de>
2023-05-27 00:05:57 +0200

    added cfgfile as first argument

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"log"
+	"os"
 
 	"github.com/naivary/instance/internal/pkg/ctrl"
 	"github.com/naivary/instance/internal/pkg/server"
@@ -13,8 +15,20 @@ func main() {
 	}
 }
 
+func getCfgFile() (string, error) {
+	if len(os.Args) < 1 {
+		return "", errors.New("missing config file as the first argument")
+	}
+	return os.Args[1], nil
+}
+
 func run() error {
-	api, err := ctrl.New("instance.yaml")
+	cfgFile, err := getCfgFile()
+	if err != nil {
+		return err
+	}
+
+	api, err := ctrl.New(cfgFile)
 	if err != nil {
 		return err
 	}
@@ -23,6 +37,5 @@ func run() error {
 	if err != nil {
 		return err
 	}
-
 	return srv.ListenAndServeTLS(api.K.String("server.crt"), api.K.String("server.key"))
 }
