775a6ca74476958e375ce972b48e26f97a491933
Sayed Mustafa Hussaini <SayedMustafaHussaini@outlook.de>
2023-05-26 15:31:41 +0200

    Pragmas not set again

--- internal/pkg/database/database.go
+++ internal/pkg/database/database.go
@@ -3,13 +3,32 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/knadh/koanf/v2"
+	"github.com/naivary/instance/internal/pkg/must"
 	_ "modernc.org/sqlite"
 )
 
+func buildDataDir(k *koanf.Koanf) (string, error) {
+	wd, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+	name := fmt.Sprintf("%s_data", k.String("name"))
+	path := filepath.Join(wd, name)
+	err = os.MkdirAll(path, os.ModePerm)
+	if err != nil {
+		return "", err
+	}
+	return path, nil
+}
+
 func buildDsn(k *koanf.Koanf) string {
-	return fmt.Sprintf("file:%s_%s.db", k.String("name"), k.String("version"))
+	dir, err := buildDataDir(k)
+	must.Must(err)
+	return fmt.Sprintf("file:%s/%s_%s.db", dir, k.String("name"), k.String("version"))
 }
 
 func initPragmas(db *sql.DB) error {
@@ -18,7 +37,7 @@ func initPragmas(db *sql.DB) error {
 		PRAGMA journal_mode       = WAL;
 		PRAGMA journal_size_limit = 200000000;
 		PRAGMA synchronous        = NORMAL;
-		PRAGMA foreign_keys       = TRUE;	
+		PRAGMA foreign_keys       = 1;	
 	`
 	_, err := db.Exec(query, nil)
 	if err != nil {
@@ -31,6 +50,9 @@ func initOptions(db *sql.DB) {
 	db.SetMaxOpenConns(1)
 }
 
+// inMem provides an in-memory
+// sqlite database which is
+// used for test purposes
 func inMem() (*sql.DB, error) {
 	db, err := sql.Open("sqlite", "file::memory:")
 	if err != nil {
@@ -48,9 +70,11 @@ func inMem() (*sql.DB, error) {
 // If `k` is nil, the database will be created in memory.
 // This should be only considered for testing puposes.
 func Connect(k *koanf.Koanf) (*sql.DB, error) {
+	// check if the connection is needed for testing purposes
 	if k == nil {
 		return inMem()
 	}
+
 	db, err := sql.Open("sqlite", buildDsn(k))
 	if err != nil {
 		return nil, err
