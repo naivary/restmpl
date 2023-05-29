656b13e07f2f72b53d484d08325f211571c00d25
Sayed Mustafa Hussaini <SayedMustafaHussaini@outlook.de>
2023-05-29 10:21:50 +0200

    starting with sys again]

--- internal/app/sys/sys.go
+++ internal/app/sys/sys.go
@@ -3,21 +3,15 @@ package sys
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/google/uuid"
-	"github.com/knadh/koanf/v2"
-	"github.com/naivary/instance/internal/pkg/models/metadata"
 	"github.com/naivary/instance/internal/pkg/register"
 	"github.com/naivary/instance/internal/pkg/service"
-	"github.com/pocketbase/dbx"
 )
 
 var _ service.Service = (*Sys)(nil)
 
 type Sys struct {
-	K    *koanf.Koanf
-	DB   *dbx.DB
+	// svcs which has to be checked
 	Svcs []service.Service
-
-	M metadata.Metadata
 }
 
 func (s Sys) Metrics() error {
